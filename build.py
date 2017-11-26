4147d13aeb9edaba4c97b2e166f1ed700d60d823
Akil Krishnan <akilram@gmail.com>
2017-11-26 09:21:44 -0600

    Use local resources, removes hotlinking of movie poster images

--- build.py
+++ build.py
@@ -7,8 +7,21 @@ Generates files in 'build' folder.
 __author__ = "Akil Krishnan"
 
 import os
-import lib.fresh_tomatoes
+import shutil
 import data.entertainment_center
+import lib.fresh_tomatoes
+
+
+# Build target directories
+
+BUILD_DIR_NAME = "build"
+RESOURCES_DIR_NAME = "resources"
+IMAGES_DIR_NAME = "images"
+
+BUILD_DIR_PATH = BUILD_DIR_NAME
+RESOURCES_DIR_PATH = "/".join([BUILD_DIR_NAME, RESOURCES_DIR_NAME])
+IMAGES_DIR_PATH = "/".join([BUILD_DIR_NAME, RESOURCES_DIR_NAME, IMAGES_DIR_NAME])
+
 
 def main():
     """
@@ -16,11 +29,34 @@ def main():
     and folder are overwritten.
     """
 
-    if not os.path.isdir("build"):
-        os.mkdir("build")
+    # Init build folder
 
-    os.chdir("build")
+    print("Checking 'build' directory...")
+
+    if not os.path.isdir(BUILD_DIR_PATH):
+        os.mkdir(BUILD_DIR_PATH)
+
+    # Build resources
+
+    print("Checking resources directory...")
+
+    if os.path.isdir(RESOURCES_DIR_PATH):
+        print("Resources directory exists. Performing cleanup.")
+        shutil.rmtree(RESOURCES_DIR_PATH, ignore_errors=True)
+
+    print("Building resources...")
+
+    shutil.copytree("resources/images", IMAGES_DIR_PATH)
+
+    # Build static page
+
+    print("Building static page...")
+
+    os.chdir(BUILD_DIR_PATH)
     lib.fresh_tomatoes.open_movies_page(data.entertainment_center.MOVIES)
+
+    print("Build complete!")
+
 
 if __name__ == "__main__":
     main()
--- data/entertainment_center.py
+++ data/entertainment_center.py
@@ -7,17 +7,17 @@ from lib.media import Movie
 MOVIES = [
     Movie(
         "The Lord of the Rings: The Fellowship of the Ring",
-        "https://images-na.ssl-images-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SY999_CR0,0,673,999_AL_.jpg",
+        "resources/images/lotr_1_movie_poster.jpg",
         "https://www.youtube.com/watch?v=Pki6jbSbXIY"
     ),
     Movie(
         "Princess Mononoke",
-        "https://upload.wikimedia.org/wikipedia/en/2/24/Princess_Mononoke_Japanese_Poster_%28Movie%29.jpg",
+        "resources/images/princess_mononoke_movie_poster.jpg",
         "https://www.youtube.com/watch?v=4OiMOHRDs14"
     ),
     Movie(
         "Sword of the Stranger",
-        "https://upload.wikimedia.org/wikipedia/en/b/b9/Sword_of_the_Stranger_poster.jpg",
+        "resources/images/sword_of_the_stranger_movie_poster.jpg",
         "https://www.youtube.com/watch?v=cyZLOYAbp4w"
     )
 ]
